015b2e9881ad7322626f6ae0fd1c52543563969c
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-05-21 14:52:18 +0700

    Initial commit

---
+++ app/Http/Controllers/DictionaryController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DictionaryController extends Controller
+{
+    public function translate(Request $request)
+    {
+        $words = [
+            'one' => 'Mot',
+            'two' => 'Hai',
+            'three' => 'Ba'
+        ];
+        $keyword = $request->keyword;
+        foreach ($words as $key => $word) {
+            if ($keyword == $key) {
+                $msg = $keyword . ':' . $word;
+                return view('dictionary', compact('msg'));
+            }
+        }
+        $msg = "not found";
+        return view('dictionary', compact('msg'));
+    }
+}
